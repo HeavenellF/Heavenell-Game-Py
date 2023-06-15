3df18132acd683b6295255a951f462848553c1da
Heavenell <Fauzirahmadiariateja@gmail.com>
2023-06-15 15:57:00 +0200

    add Image for Background

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ from sys import exit
 
 width = 800
 height = 400
+fps = 60
 
 pygame.init()
 screen = pygame.display.set_mode((width,height))
-pygame.display.set_caption('Game Name Here')
+pygame.display.set_caption('Game Name Here')    # Window Name here
 clock = pygame.time.Clock()
+test_font = pygame.font.Font(None, 50)
+
+surface_background = pygame.image.load('image/background1.jpg')
+surface_gameTitle = test_font.render('Robot Assistant', True, 'White')
+
 
 while True:
     for event in pygame.event.get():
@@ -15,5 +21,8 @@ while True:
             pygame.quit()
             exit()
     
+    screen.blit(surface_background,(100,0))
+    screen.blit(surface_gameTitle,(275, 30))
+
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(fps)
