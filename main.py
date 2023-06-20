8a32575cc479e31d9ec4f5087209420bc82120f0
Heavenell <Fauzirahmadiariateja@gmail.com>
2023-06-20 19:01:49 +0200

    add Font

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from sys import exit
 
-width = 800
+width = 600
 height = 400
 fps = 60
 
@@ -9,13 +9,16 @@ pygame.init()
 screen = pygame.display.set_mode((width,height))
 pygame.display.set_caption('Game Name Here')    # Window Name here
 clock = pygame.time.Clock()
-test_font = pygame.font.Font(None, 50)
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 background_surf = pygame.image.load('image/background1.jpg').convert()
+
 gameTitle_surf = test_font.render('Robot Assistant', True, 'White')
+gameTitle_rect = gameTitle_surf.get_rect(midtop=(width/2,30))
 
 player_surf = pygame.image.load('image/player.png').convert_alpha()
-player_rect = player_surf.get_rect(midbottom= (400,350))
+player_rect = player_surf.get_rect(midbottom= (width/2,350))
+
 #pygame.transform.flip(surface_background, True, False)
 
 
@@ -24,9 +27,11 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print(event.pos)
     
-    screen.blit(background_surf,(100,0))
-    screen.blit(gameTitle_surf,(275, 30))
+    screen.blit(background_surf,(0,0))
+    screen.blit(gameTitle_surf,gameTitle_rect)
 
     screen.blit(player_surf,player_rect)
 
