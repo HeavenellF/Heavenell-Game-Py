8d9ddd867874ca600a4a3f2266887868cceb31a2
Heavenell <Fauzirahmadiariateja@gmail.com>
2023-07-13 06:43:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -27,13 +27,19 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        # click to get the Cord of the Cursor
         if event.type == pygame.MOUSEBUTTONDOWN:
             print(event.pos)
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                print('Jump')
     
     screen.blit(background_surf,(0,0))
     screen.blit(gameTitle_surf,gameTitle_rect)
 
     screen.blit(player_surf,player_rect)
+
 
     pygame.display.update()
     clock.tick(fps)
