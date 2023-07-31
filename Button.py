4c8c3cbc7ff27cc5734862cc1f48504f0255b88e
Heavenell <Fauzirahmadiariateja@gmail.com>
2023-07-31 07:10:22 +0200

    change mainMenu.py to Button.py

---
+++ Button.py
@@ -0,0 +1,63 @@
+import pygame
+
+def mainMenu_elements(width, height, font1, font3, button_surf1):
+    gameTitle_surf = font1.render('Agent J', True, 'Black')
+    gameTitle_rect = gameTitle_surf.get_rect(midtop=(width / 2, 40))
+
+    button_rectPlay = button_surf1.get_rect(midtop=(width / 2, height / 2 - 90))
+    button_rectSetting = button_surf1.get_rect(midtop=(width / 2, height / 2))
+    button_rectExit = button_surf1.get_rect(midtop=(width / 2, height / 2 + 90))
+
+    buttonPlay_surf = font3.render('Play', True, 'Black')
+    buttonPlay_rect = buttonPlay_surf.get_rect(midtop=(width / 2, height / 2 - 85))
+
+    buttonSetting_surf = font3.render('Setting', True, 'Black')
+    buttonSetting_rect = buttonSetting_surf.get_rect(midtop=(width / 2, height / 2 + 5))
+
+    buttonExit_surf = font3.render('Exit', True, 'Black')
+    buttonExit_rect = buttonExit_surf.get_rect(midtop=(width / 2, height / 2 + 95))
+
+    return {
+        'gameTitle_surf': gameTitle_surf,
+        'gameTitle_rect': gameTitle_rect,
+        'button_rectPlay': button_rectPlay,
+        'button_rectSetting': button_rectSetting,
+        'button_rectExit': button_rectExit,
+        'buttonPlay_surf': buttonPlay_surf,
+        'buttonPlay_rect': buttonPlay_rect,
+        'buttonSetting_surf': buttonSetting_surf,
+        'buttonSetting_rect': buttonSetting_rect,
+        'buttonExit_surf': buttonExit_surf,
+        'buttonExit_rect': buttonExit_rect,
+    }
+
+def gameOver_elements(width, height, font1, font3, button_surf1):
+    gameOver_surf = font3.render('You\'ve Completed the Game!', True, 'Black')
+    gameOver_rect = gameOver_surf.get_rect(midtop=(width / 2, 50))
+
+    button_rectPlayAgain = button_surf1.get_rect(midtop=(width / 2, height / 2 - 90))
+    button_rectMainMenu = button_surf1.get_rect(midtop=(width / 2, height / 2))
+    button_rectExit = button_surf1.get_rect(midtop=(width / 2, height / 2 + 90))
+
+    buttonPlayAgain_surf = font3.render('Play Again', True, 'Black')
+    buttonPlayAgain_rect = buttonPlayAgain_surf.get_rect(midtop=(width / 2, height / 2 - 85))
+
+    buttonMainMenu_surf = font3.render('Main Menu', True, 'Black')
+    buttonMainMenu_rect = buttonMainMenu_surf.get_rect(midtop=(width / 2, height / 2 + 5))
+
+    buttonExit_surf = font3.render('Exit', True, 'Black')
+    buttonExit_rect = buttonExit_surf.get_rect(midtop=(width / 2, height / 2 + 95))
+
+    return {
+        'gameOver_surf': gameOver_surf,
+        'gameOver_rect': gameOver_rect,
+        'button_rectPlayAgain': button_rectPlayAgain,
+        'button_rectMainMenu': button_rectMainMenu,
+        'button_rectExit': button_rectExit,
+        'buttonPlayAgain_surf': buttonPlayAgain_surf,
+        'buttonPlayAgain_rect': buttonPlayAgain_rect,
+        'buttonMainMenu_surf': buttonMainMenu_surf,
+        'buttonMainMenu_rect': buttonMainMenu_rect,
+        'buttonExit_surf': buttonExit_surf,
+        'buttonExit_rect': buttonExit_rect,
+    }
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pygame
 from sys import exit
 
-from mainMenu import mainMenu_elements
+from Button import mainMenu_elements, gameOver_elements
 
-def display_time():
+def display_time(finish):
     current_time = pygame.time.get_ticks() - start_time - pause_duration
     minutes = current_time // 60000
     seconds = (current_time % 60000) // 1000
@@ -16,6 +16,14 @@ def display_time():
     time_surf = font2.render(f'{time_str}',False,'Black')
     time_rect = time_surf.get_rect(center = (width - 700,25))
     screen.blit(time_surf,time_rect)
+
+    if finish:
+        # Create the time finish string
+        timefinish_str = f'Your Time: {minutes_str}:{seconds_str}'
+        timefinish_surf = font3.render(timefinish_str, True, 'Black')
+        timefinish_rect = timefinish_surf.get_rect(center=(width / 2, 150))
+
+        return timefinish_surf, timefinish_rect
 
 width = 800
 height = 600
@@ -34,10 +42,13 @@ pause_duration = 0
 pause_start = 0
 pause_end = 0
 game_state = 0
+glint_button = None
+finish = False
 
 background_surf = pygame.image.load('image/background1.jpg').convert()
 
-button_surf = pygame.image.load('image/misc/ButtonMain.png').convert()
+button_surf1 = pygame.image.load('image/misc/ButtonMain.png').convert()
+
 
 
 player_surf = pygame.image.load('image/player.png').convert_alpha()
@@ -45,8 +56,8 @@ player_rect = player_surf.get_rect(midbottom= (width/2,350))
 player_gravity = 0
 player_direction = 0
 
-mainMenu_elements = mainMenu_elements(width, height, font1, font3, button_surf)
-
+mainMenu_elements = mainMenu_elements(width, height, font1, font3, button_surf1)
+gameOver_elements = gameOver_elements(width, height, font1, font3, button_surf1)
 
 #pygame.transform.flip(surface_background, True, False)
 
@@ -96,10 +107,28 @@ while True:
         
         # gameover
         elif game_state == 3:
-            if event.type == pygame.KEYDOWN:
-                player_rect.bottom = 350
-                game_state = 1
-                start_time = pygame.time.get_ticks()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # Play Again
+                if gameOver_elements['button_rectPlayAgain'].collidepoint(event.pos):
+                    game_state = 1
+                    start_time = pygame.time.get_ticks()
+                # Main Menu
+                if gameOver_elements['button_rectMainMenu'].collidepoint(event.pos):
+                    game_state = 0
+                # Exit
+                if gameOver_elements['button_rectExit'].collidepoint(event.pos):
+                    pygame.quit()
+                    exit()
+            # Glinting Effect
+            if event.type == pygame.MOUSEMOTION:
+                if gameOver_elements['button_rectPlayAgain'].collidepoint(event.pos):
+                    glint_button = 'play again'
+                elif gameOver_elements['button_rectMainMenu'].collidepoint(event.pos):
+                    glint_button = 'main menu'
+                elif gameOver_elements['button_rectExit'].collidepoint(event.pos):
+                    glint_button = 'exit'
+                else:
+                    glint_button = None
         
         # Main Menu
         elif game_state == 0:         
@@ -112,6 +141,16 @@ while True:
                 if mainMenu_elements['button_rectExit'].collidepoint(event.pos):
                     pygame.quit()
                     exit()
+            # Glinting Effect
+            if event.type == pygame.MOUSEMOTION:
+                if mainMenu_elements['button_rectPlay'].collidepoint(event.pos):
+                    glint_button = 'play'
+                elif mainMenu_elements['button_rectSetting'].collidepoint(event.pos):
+                    glint_button = 'setting'
+                elif mainMenu_elements['button_rectExit'].collidepoint(event.pos):
+                    glint_button = 'exit'
+                else:
+                    glint_button = None
 
                     
 
@@ -119,7 +158,7 @@ while True:
     if game_state == 1:
         # Title and Background
         screen.blit(background_surf,(0,0))
-        display_time()
+        display_time(finish)
 
         # Player
         player_gravity += 0.5
@@ -130,6 +169,8 @@ while True:
         screen.blit(player_surf,player_rect)
 
         if player_rect.top <= 0:
+            player_rect.bottom = 350
+            timefinish_surf,timefinish_rect = display_time(finish=True)
             game_state = 3
     
     # Pause
@@ -143,19 +184,34 @@ while True:
     
     # Gameover
     elif game_state == 3:
-        screen.fill('Red')
+        screen.fill('white')
+        screen.blit(gameOver_elements['gameOver_surf'], gameOver_elements['gameOver_rect'])
+        screen.blit(timefinish_surf, timefinish_rect)
+
+        screen.blit(button_surf1, gameOver_elements['button_rectPlayAgain'])
+        screen.blit(gameOver_elements['buttonPlayAgain_surf'], gameOver_elements['buttonPlayAgain_rect'])
+        screen.blit(button_surf1, gameOver_elements['button_rectMainMenu'])
+        screen.blit(gameOver_elements['buttonMainMenu_surf'], gameOver_elements['buttonMainMenu_rect'])
+        screen.blit(button_surf1, gameOver_elements['button_rectExit'])
+        screen.blit(gameOver_elements['buttonExit_surf'], gameOver_elements['buttonExit_rect'])
+        if glint_button == 'play again': pygame.draw.rect(screen, 'Black', gameOver_elements['button_rectPlayAgain'], 4)
+        elif glint_button == 'main menu': pygame.draw.rect(screen, 'Black', gameOver_elements['button_rectMainMenu'], 4)
+        elif glint_button == 'exit': pygame.draw.rect(screen, 'Black', gameOver_elements['button_rectExit'], 4)
 
     # Main Menu
     elif game_state == 0:
         screen.fill('white')
-
         screen.blit(mainMenu_elements['gameTitle_surf'], mainMenu_elements['gameTitle_rect'])
-        screen.blit(button_surf, mainMenu_elements['button_rectPlay'])
+
+        screen.blit(button_surf1, mainMenu_elements['button_rectPlay'])
         screen.blit(mainMenu_elements['buttonPlay_surf'], mainMenu_elements['buttonPlay_rect'])
-        screen.blit(button_surf, mainMenu_elements['button_rectSetting'])
+        screen.blit(button_surf1, mainMenu_elements['button_rectSetting'])
         screen.blit(mainMenu_elements['buttonSetting_surf'], mainMenu_elements['buttonSetting_rect'])
-        screen.blit(button_surf, mainMenu_elements['button_rectExit'])
+        screen.blit(button_surf1, mainMenu_elements['button_rectExit'])
         screen.blit(mainMenu_elements['buttonExit_surf'], mainMenu_elements['buttonExit_rect'])
+        if glint_button == 'play': pygame.draw.rect(screen, 'Black', mainMenu_elements['button_rectPlay'], 4)
+        elif glint_button == 'setting': pygame.draw.rect(screen, 'Black', mainMenu_elements['button_rectSetting'], 4)
+        elif glint_button == 'exit': pygame.draw.rect(screen, 'Black', mainMenu_elements['button_rectExit'], 4)
 
 
     pygame.display.update()
